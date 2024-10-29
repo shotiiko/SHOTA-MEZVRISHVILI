3852d0d0d74ac78aa93927551254e418ff0ee262
Shota Mezvrishvili <shmezvrishvili@credo.ge>
2024-10-30 03:18:19 +0400

    ADDED.TASKTHREE

---
+++ task three/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+int input = int.Parse(Console.ReadLine());
+Random random = new Random();
+int randomNumber = random.Next(1, 10);
+if (input < 1 || input > 10);
+else if (input != randomNumber);
+ Console.WriteLine("ახლიდან სცადე");
+else Console.WriteLine("ციფრი გამოიცანით");
+
+
+
