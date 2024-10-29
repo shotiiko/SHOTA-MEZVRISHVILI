df89d729c8a9181e667cac1c274ca4ae47a157f4
Shota Mezvrishvili <shmezvrishvili@credo.ge>
2024-10-30 01:01:56 +0400

    added.task2

---
+++ task two/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int age = 25;
+int HEIGHT = 185;
+float WEIGHT = 84.5F;
+String name = "SHOTIKO";
+Console.WriteLine(age);
+Console.WriteLine(HEIGHT);
+Console.WriteLine(WEIGHT);
+Console.WriteLine(name);
