bdb7221b418eaf68cf9eaea75d7648840cf1f59a
Shota Mezvrishvili <shmezvrishvili@credo.ge>
2024-11-07 22:43:00 +0400

    added

---
+++ task five/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("datseret pirveli studentis sakheli");
+string first = Console.ReadLine();
+Console.WriteLine("datseret meore studentis sakheli");
+string second = Console.ReadLine();
+Console.WriteLine("datseret mesame studentis sakheli");
+string third = Console.ReadLine();
+string[] students = { first, second, third };
+Console.WriteLine("kvela studentis sakhelia");
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
+
+int[,] grades = new int[3,2];
+
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        Console.WriteLine("sheiyvanet " + (i+1) +" studentis "+(j+1) +" qula");
+        grades[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (int i = 0;i < 3;i++)
+{
+    Console.WriteLine(students[i] + " " + grades[i,0] + " " + grades[i,1]);
+}
+
+
+
--- task four/task 4/task 4/Program.cs
+++ task four/task 4/task 4/Program.cs
@@ -1,35 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-int input = int.Parse(Console.ReadLine());
-if (input >= 1 && input <= 7)
+
+//int input = int.Parse(Console.ReadLine());
+//if (input >= 1 && input <= 7)
+//{
+
+//    switch (input)
+//    {
+//        case 6:
+//            Console.WriteLine("kvira");
+//            break;
+//        case 1:
+//            Console.WriteLine("orshabati");
+//            break;
+
+//        case 2:
+//            Console.WriteLine("samshabati");
+//            break;
+
+//        case 3:
+//            Console.WriteLine("otkhshabati");
+//            break;
+//        case 4:
+//            Console.WriteLine("khutshabati");
+//            break;
+//        case 5:
+//            Console.WriteLine("paraskevi");
+//            break;
+//        default:
+//            Console.WriteLine("shabati");
+//            break;
+//    }
+//}
+// else  Console.WriteLine("rickhvi arastsoria");
+
+// მეორე დავალება
+
+//int jami = 0;
+//while (true)
+//{
+//    Console.WriteLine("sheikvane ritskhvi: ");
+//    int input2 = int.Parse(Console.ReadLine());
+
+//    if (input2 < 0)
+//    {
+//        Console.WriteLine("dajamebuli ritskhvia:" + jami);
+//        break;
+//    }
+//    jami = jami + input2;
+//}
+//Console.WriteLine("shekreba dasrulebulia");
+
+//მესამე დავალება
+
+Console.WriteLine("sheikvane ritskhvi 1dan 10mde: ");
+int input4 = int.Parse(Console.ReadLine());
+Console.WriteLine("migebuli namravli");
+for (int i = 1; i <= 10; i++)
 {
-
-    switch (input)
+    int namravli = (i * input4);
     {
-        case 6:
-            Console.WriteLine("kvira");
-            break;
-        case 1:
-            Console.WriteLine("orshabati");
-            break;
-
-        case 2:
-            Console.WriteLine("samshabati");
-            break;
-
-        case 3:
-            Console.WriteLine("otkhshabati");
-            break;
-        case 4:
-            Console.WriteLine("khutshabati");
-            break;
-        case 5:
-            Console.WriteLine("paraskevi");
-            break;
-        default:
-            Console.WriteLine("shabati");
-            break;
+        Console.WriteLine($"{i} * {input4} = {namravli}");
     }
 }
- else  Console.WriteLine("rickhvi arastsoria");
+
+
+
+
