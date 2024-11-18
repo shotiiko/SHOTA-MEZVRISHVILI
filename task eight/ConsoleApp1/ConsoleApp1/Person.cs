8ef77a45cb82eccbc0f57ab10fa3f300902a8cfa
Shota Mezvrishvili <shmezvrishvili@credo.ge>
2024-11-18 16:23:39 +0400

    added

---
+++ task eight/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}
+
---
+++ task eight/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+class Program
+{
+    static void Main()
+    {
+        string personName = "Shota";
+        int personAge = 25;
+
+        Person person1 = new Person(personAge, personName);
+
+        Person person2 = person1;
+        person2.Name = "Nika";
+        person2.Age = 37;
+
+        ChangePerson(person1);
+
+        Console.WriteLine(person1.Name + " " + person1.Age);
+        Console.WriteLine(person2.Name + " " + person2.Age);
+    }
+
+    static void ChangePerson(Person p)
+    {
+        p.Name = "Bela";
+        p.Age++;
+    }
+}
