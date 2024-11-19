86b0c5252af8fe8efc6a357bfe9fcf269542fc14
Shota Mezvrishvili <shmezvrishvili@credo.ge>
2024-11-19 22:54:07 +0400

    added

---
+++ task six/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("sheikvanet mreli ritskhvi");
+
+int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+     array[i] = input;
+}
+
+Console.WriteLine("sheikvanet masivi");
+int index = int.Parse(Console.ReadLine());
+
+int sum = sumritskhvi(array, index);
+Console.WriteLine("sumi" + sum);
+
+static int sumritskhvi(int[] array, int index)
+{
+    int arrayNumber = array[index];
+    int sum = 0;
+    while (arrayNumber > 0)
+    {
+        sum += arrayNumber % 10;
+        arrayNumber /= 10;
+    }
+    return sum;
+}
